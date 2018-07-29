7dfc614d5e1b03c0f4ae46942b889809e6927e1e
Jordan Krueger <jkrueger702@gmail.com>
2018-07-29 04:20:37 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,24 @@
 def starts_with_a_vowel?(word)
-  string.scan()\b[Uu]n[a-z]*ing\b)
+word.match(/\A[aeiouAEIOU]/) != nil
 end
+
+starts_with_a_vowel?("!ord")
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/un\w*ing/)
 end
+
+words_starting_with_un_and_ending_with_ing("unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary")
 
 def words_five_letters_long(text)
-
+text.scan(/\b\w{5}\b/)
 end
+
+
+words_five_letters_long("extreme briny crepe parking snaps grouping snafu round dog be fork spoon")
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+text.match(/\A[A-Z]\w*\W\b/) != nil
 end
 
-def valid_phone_number?(phone)
-
-end
+first_word_capitalized_and_ends_with_punctuation?("I stepped on a Corn Flake, now I'm a cereal killer.")
