0d2e32dd7a8c4ff7f05dd417e4a4a6ffea1bf63f
Jordan Krueger <jkrueger702@gmail.com>
2018-07-29 04:21:36 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,24 +1,20 @@
 def starts_with_a_vowel?(word)
-word.match(/\A[aeiouAEIOU]/) != nil
-end
+word.match(/\A[aeiouAEIOU]/) 
 
-starts_with_a_vowel?("!ord")
+end
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.scan(/un\w*ing/)
-end
 
-words_starting_with_un_and_ending_with_ing("unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary")
+end
 
 def words_five_letters_long(text)
-text.scan(/\b\w{5}\b/)
+
 end
-
-
-words_five_letters_long("extreme briny crepe parking snaps grouping snafu round dog be fork spoon")
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.match(/\A[A-Z]\w*\W\b/) != nil
+
 end
 
-first_word_capitalized_and_ends_with_punctuation?("I stepped on a Corn Flake, now I'm a cereal killer.")
+def valid_phone_number?(phone)
+
+end
